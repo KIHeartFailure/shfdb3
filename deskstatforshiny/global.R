d865ed8425424671fb0b11259b477269288a9ba2
Lina Benson <lina.benson@ki.se>
2021-06-07 14:12:40 +0200

    mod coverage in accordence w new numbers

--- deskstatforshiny/global.R
+++ deskstatforshiny/global.R
@@ -11,7 +11,7 @@ library(lubridate)
 library(ggplot2)
 
 # delete when external
-#setwd("C:/Users/Lina/STATISTIK/Projects/20200225_shfdb3/deskstatshiny")
+#setwd("./deskstatforshiny")
 
 source("R/plot_func.R")
 source("R/cols.R")
